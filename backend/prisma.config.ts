2f28ca7295de63acda7cdab1b09d97a1eed32b6c
Socheat-Hai <socheat.hai@student.cadt.edu.kh>
2026-06-23 12:42:49 +0700

    change some part

--- backend/prisma.config.ts
+++ backend/prisma.config.ts
@@ -2,13 +2,14 @@ import { defineConfig } from "prisma/config";
 import "dotenv/config";
 
 export default defineConfig({
+   migrations: {
+    seed: 'node prisma/seed.js',
+  },
+  
   schema: "prisma/schema.prisma",
 
   datasource: {
     url: process.env.DATABASE_URL!,
   },
 
-  migrations: {
-    seed: 'node prisma/seed.js',
-  },
 });
